f9d577fe8d6905c25848a1acdae5a1a2caf94bf8
bmox <62886280+bmox@users.noreply.github.com>
2022-01-10 18:33:29 +0530

    Add files via upload

---
+++ monitor.py
@@ -0,0 +1,176 @@
+import os
+import sys
+import socket
+import time
+import datetime as dt
+
+FILE = os.path.join(os.getcwd(), "networkinfo.log")
+
+
+from datetime import datetime
+import pytz
+def get_time():
+  UTC = pytz.utc
+  IST = pytz.timezone("Asia/Kolkata")
+  datetime_ist = datetime.now(IST)
+  current_time=str(datetime_ist.strftime("%I:%M:%S %p"))
+  date=str(datetime_ist.strftime('%d:%m:%Y'))
+  return date,current_time 
+
+
+def ping():
+	# to ping a particular IP
+	try:
+		socket.setdefaulttimeout(3)
+		
+		# if data interruption occurs for 3
+		# seconds, <except> part will be executed
+
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		# AF_INET: address family
+		# SOCK_STREAM: type for TCP
+
+		host = "8.8.8.8"
+		port = 53
+
+		server_address = (host, port)
+		s.connect(server_address)
+
+	except OSError as error:
+		return False
+		# function returns false value
+		# after data interruption
+
+	else:
+		s.close()
+		# closing the connection after the
+		# communication with the server is completed
+		return True
+
+
+def calculate_time(start, stop):
+
+	# calculating unavailabilty
+	# time and converting it in seconds
+	difference = stop - start
+	seconds = float(str(difference.total_seconds()))
+	return str(dt.timedelta(seconds=seconds)).split(".")[0]
+
+
+
+def first_check():
+	# to check if the system was already
+	# connected to an internet connection
+
+	if ping():
+		# if ping returns true
+		live = "\nCONNECTION ACQUIRED\n"
+		print(live)
+		connection_acquired_time = datetime.now()
+		d,t=get_time()
+		acquiring_message = f"connection acquired at: {d} {t}" 
+		print(acquiring_message)
+
+		with open(FILE, "a") as file:
+		
+			# writes into the log file
+			file.write(live)
+			file.write(acquiring_message)
+
+		return True
+
+	else:
+		# if ping returns false
+		not_live = "\nCONNECTION NOT ACQUIRED\n"
+		print(not_live)
+
+		with open(FILE, "a") as file:
+		
+			# writes into the log file
+			file.write(not_live)
+		return False
+
+
+def main():
+
+	# main function to call functions
+	monitor_start_time =datetime.now()
+	d,t=get_time()
+	monitoring_date_time = f"monitoring started at: {d} {t}"
+
+	if first_check():
+		# if true
+		print(monitoring_date_time)
+		# monitoring will only start when
+		# the connection will be acquired
+
+	else:
+		# if false
+		while True:
+		
+			# infinite loop to see if the connection is acquired
+			if not ping():
+				
+				# if connection not acquired
+				time.sleep(1)
+			else:
+				
+				# if connection is acquired
+				first_check()
+				print(monitoring_date_time)
+				break
+
+	with open(FILE, "a") as file:
+	
+		# write into the file as a into networkinfo.log,
+		# "a" - append: opens file for appending,
+		# creates the file if it does not exist???
+		file.write("\n")
+		file.write(monitoring_date_time + "\n")
+
+	while True:
+	
+		# infinite loop, as we are monitoring
+		# the network connection till the machine runs
+		if ping():
+			
+			# if true: the loop will execute after every 5 seconds
+			time.sleep(5)
+
+		else:
+			# if false: fail message will be displayed
+			down_time = datetime.now()
+			d,t=get_time()
+   
+			fail_msg = f"disconnected at: {d} {t} "
+			print(fail_msg)
+
+			with open(FILE, "a") as file:
+				# writes into the log file
+				file.write(fail_msg + "\n")
+
+			while not ping():
+			
+				# infinite loop, will run till ping() return true
+				time.sleep(1)
+
+			up_time =datetime.now()
+			
+			# after loop breaks, connection restored
+			d,t=get_time()
+			uptime_message = f"connected again: {d} {t}"
+
+			down_time = calculate_time(down_time, up_time)
+			unavailablity_time = "connection was unavailable for: " + down_time
+
+			print(uptime_message)
+			print(unavailablity_time)
+
+			with open(FILE, "a") as file:
+				
+				# log entry for connection restoration time,
+				# and unavailability time
+				file.write(uptime_message + "\n")
+				file.write(unavailablity_time + "\n")
+
+main()
---
+++ save_as_csv.py
@@ -0,0 +1,76 @@
+import os
+from csv import writer
+FILE = os.path.join(os.getcwd(), "networkinfo.log")
+csv_file=os.path.join(os.getcwd(), "networkinfo.csv")
+
+template=["Date","Disconnected at","Connected at","Disconnected duration"]
+with open(csv_file, 'w',newline="") as f_object:
+  writer_object = writer(f_object)
+  writer_object.writerow(template)
+  f_object.close()
+
+file = open(FILE, 'r')
+lines = file.read().splitlines()
+file.close()
+get_index=[]
+for index, line in enumerate(lines):
+  if line=='':
+    get_index.append(index)
+new_list=[] 
+for i in range(len(get_index)):
+  if i!=len(get_index)-1:
+    new_list.append(lines[get_index[i]:get_index[i+1]])
+  else:
+    new_list.append(lines[get_index[-1]:])
+
+
+dict_writer={"Date":"",
+             "Connected_at":"",
+             "Disconnected_at":"",
+             "Disconnected_duration":""}
+
+
+j=0
+for line in new_list:
+
+  for i in line:
+    
+    if i=='CONNECTION ACQUIRED':
+      pass
+    if i.startswith("monitoring started at:"):
+      st=i
+      st=st.split("monitoring started at: ")
+      data=st[-1].split(" ")
+      date_format=data[0].replace(":","/")
+      dict_writer["Date"]=date_format
+    elif i.startswith("disconnected at:")==True:
+      st=i
+      st=st.split("disconnected at:")
+      st=st[-1].split(" ")
+      dict_writer["Disconnected_at"]=st[2]+" "+st[3]
+      j=1
+    elif i.startswith("connected again")==True:
+      st=i
+      st=st.split("connected again:")
+      st=st[-1].split(" ")
+      dict_writer["Connected_at"]=st[2]+" "+st[3]
+      j=2
+    
+    elif i.startswith("connection was unavailable for:")==True:
+      st=i
+      st=st.split("connection was unavailable for: ")
+      st=st[-1]
+      dict_writer["Disconnected_duration"]=st
+      j=3
+    if j==3:
+      create_list=list(dict_writer.values())
+      with open(csv_file, 'a',newline="") as f_object:
+        writer_object = writer(f_object)
+        writer_object.writerow(create_list)
+        f_object.close()
+      j=0
+  with open(csv_file, 'a',newline="") as f_object:
+    writer_object = writer(f_object)
+    writer_object.writerow("")
+    f_object.close()
+
